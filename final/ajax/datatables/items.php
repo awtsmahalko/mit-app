cd30eb132130e07f8a560625699848ade532aea2
awtsmahalko <56856915+awtsmahalko@users.noreply.github.com>
2021-11-18 11:47:30 +0800

    Update items.php

--- final/ajax/datatables/items.php
+++ final/ajax/datatables/items.php
@@ -1,20 +1,20 @@
 <?php
 require_once '../../core/config.php';
 
-$fetch = $mysqli_connect->query("SELECT * from tbl_items ORDER BY item_name ASC") or die($mysqli_connect->error);
+//$fetch = $mysqli_connect->query("SELECT * from tbl_items ORDER BY item_name ASC") or die($mysqli_connect->error);
 
 $response['data'] = array();
 
-while( $row = $fetch->fetch_array() ){
-	$list = array();
-	
-	$list['id'] = $row['item_id'];
-	$list['item_name'] = $row['item_name'];
-	$list['item_desc'] = $row['item_desc'];
-	$list['item_serial_no'] = $row['item_serial_no'];
-	$list['date_modified'] = date('M d, Y h:i A', strtotime($row['date_modified']));
+// while( $row = $fetch->fetch_array() ){
+// 	$list = array();
 
-	array_push($response['data'], $list);
-}
+// 	$list['id'] = $row['item_id'];
+// 	$list['item_name'] = $row['item_name'];
+// 	$list['item_desc'] = $row['item_desc'];
+// 	$list['item_serial_no'] = $row['item_serial_no'];
+// 	$list['date_modified'] = date('M d, Y h:i A', strtotime($row['date_modified']));
+
+// 	array_push($response['data'], $list);
+// }
 
 echo json_encode($response);
