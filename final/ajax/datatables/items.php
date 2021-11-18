9c7d3e34feef29b28917a3a3221da273bcf3d393
awtsmahalko <56856915+awtsmahalko@users.noreply.github.com>
2021-11-18 13:52:52 +0800

    Update items.php

--- final/ajax/datatables/items.php
+++ final/ajax/datatables/items.php
@@ -8,8 +8,8 @@ $response['data'] = array();
 while ($row = $fetch->fetch_array()) {
 	$list = array();
 
-	$list['id'] = $row['item_id'];
-	$list['item_name'] = $row["item_name"];
+	$list['id'] = var_dump($row);
+	$list['item_name'] = ""; //$row["item_name"];
 	$list['item_desc'] = ""; //$row['item_desc'];
 	$list['item_serial_no'] = ""; // $row['item_serial_no'];
 	$list['date_modified'] = ""; //date('M d, Y h:i A', strtotime($row['date_modified']));
